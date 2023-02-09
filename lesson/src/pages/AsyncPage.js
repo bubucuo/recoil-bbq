ba2e2bd204d3ddba5d77dc990a1343f26347bab4
bubucuo <276185876@qq.com>
2023-02-09 22:18:25 +0800

    初始化 异步

--- lesson/src/pages/AsyncPage.js
+++ lesson/src/pages/AsyncPage.js
@@ -1,19 +1,19 @@
-import {
-  atom,
-  selectorFamily,
-  selector,
-  useRecoilValue,
-  useRecoilState,
-} from "recoil";
-import {Suspense} from "react";
+import {atom, selectorFamily, useRecoilState} from "recoil";
+import {useState, Suspense} from "react";
 
 export default function AsyncPage(props) {
+  const [userId, setUserId] = useState(1);
+
+  const refresh = () => {
+    setUserId(userId + 1);
+  };
   return (
     <div>
       <h3>AsyncPage</h3>
 
+      <button onClick={refresh}>refresh</button>
       <Suspense fallback={<h1>loading</h1>}>
-        <UserData />
+        <UserData userId={userId} />
       </Suspense>
     </div>
   );
@@ -23,42 +23,22 @@ const userAtomState = atom({key: "userAtom", default: {}});
 
 const userState = selectorFamily({
   key: "user",
-  get: (userId: number) => async () => {
-    const userData = await fetch(`https://randomuser.me/api`).then((res) =>
-      res.json()
-    );
-    if (userId === 4) throw new Error("User does not exist");
+  get:
+    ({userId}) =>
+    async () => {
+      const userData = await fetch(`https://randomuser.me/api`).then((res) =>
+        res.json()
+      );
 
-    return userData.results[0];
-  },
-  set: ({set, get}, value) => {
-    console.log(
-      "%c [  ]-16",
-      "font-size:13px; background:pink; color:#bf2c9f;",
-      value
-    );
-  },
+      return userData.results[0];
+    },
 });
 
-function UserData(props) {
-  // const user = useRecoilValue(userState());
-
-  const [user, setUser] = useRecoilState(userState());
-
-  if (!user) {
-    return null;
-  }
+function UserData({userId}) {
+  const [user, setUser] = useRecoilState(userState(userId));
 
   return (
     <div>
-      <h4>User</h4>
-      <button
-        onClick={() => {
-          setUser(userState());
-        }}>
-        refresh user data
-      </button>
-
       <p>name: {user.name.first}</p>
     </div>
   );
